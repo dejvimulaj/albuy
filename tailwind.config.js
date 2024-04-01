8250e34092a05203e0bea03b71cb79493fb77415
dejvimulaj <davemulaj@gmail.com>
2024-04-01 02:38:11 +0200

    Initial Setup Boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.neutral,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+    }
+  },
+  plugins: [],
+}
+
